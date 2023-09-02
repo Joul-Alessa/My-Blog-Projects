7c61eeae51fe33af958d1733871a19bc29cfe2a4
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2023-09-01 18:36:42 -0600

    Preparing repository

--- MiAprendizaje/scripts.js
+++
@@ -1,8 +0,0 @@
-function toggle(id) {
-    var elemento = document.getElementById(id);
-    if (elemento.style.display === "none") {
-        elemento.style.display = "block";
-    } else {
-        elemento.style.display = "none";
-    }
-}
