a127e43c82ac8b992264c7ff122a0f54a96e90c2
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2024-11-20 20:47:47 -0600

    Changed find one videogame series from id to slug

--- backend/my-gamer-room-backend/src/api/videogame-serie/controllers/videogame-serie.js
+++ backend/my-gamer-room-backend/src/api/videogame-serie/controllers/videogame-serie.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::videogame-serie.videogame-serie');
+module.exports = createCoreController('api::videogame-serie.videogame-serie', ({strapi}) => ({
+    async findOne(ctx) {
+        const { slug } = ctx.params;
+
+        const entity = await strapi.db.query('api::videogame-serie.videogame-serie').findOne({
+            where: { slug }
+        });
+
+        const sanitizedEntity = await this.sanitizeOutput(entity);
+        return this.transformResponse(sanitizedEntity);
+    }
+}));
