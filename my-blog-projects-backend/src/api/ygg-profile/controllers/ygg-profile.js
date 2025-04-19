87f1b7d415a11e1eccfdf6682b3a5ba29d969da1
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2025-04-18 22:49:56 -0600

    Added findOne logic to Yggdrasil Profiles controller

--- my-blog-projects-backend/src/api/ygg-profile/controllers/ygg-profile.js
+++ my-blog-projects-backend/src/api/ygg-profile/controllers/ygg-profile.js
@@ -6,4 +6,33 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::ygg-profile.ygg-profile');
+module.exports = createCoreController('api::ygg-profile.ygg-profile', ({ strapi }) => ({
+  async findBySlug(ctx){
+    const { slug } = ctx.params;
+
+    const { query } = ctx;
+
+    const locale = query.locale === undefined
+      ? 'en'
+      : query.locale;
+
+    const entity = await strapi.db.query('api::ygg-profile.ygg-profile').findOne({
+      where: {
+        slug,
+        locale
+      }
+    });
+
+    if(entity != undefined)
+    {
+      delete entity.id;
+      delete entity.documentId;
+      delete entity.createdAt;
+      delete entity.updatedAt;
+      delete entity.publishedAt;
+    }
+
+    const sanitizedEntity = await this.sanitizeOutput(entity);
+    return this.transformResponse(sanitizedEntity);
+  }
+}));
