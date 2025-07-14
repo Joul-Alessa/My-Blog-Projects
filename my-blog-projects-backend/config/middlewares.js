cc84e5b416fe0206aa4dafe668fa42bbf4d11ccf
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2025-07-13 21:43:03 -0600

    Changed files for configuration purposes in production

--- my-blog-projects-backend/config/middlewares.js
+++ my-blog-projects-backend/config/middlewares.js
@@ -2,7 +2,15 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://joul-alessa.dev/'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true
+    }
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
